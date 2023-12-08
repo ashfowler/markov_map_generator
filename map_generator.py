aee2738e93abed6e4df1a8afb54e09800c687bfa
Ash Fowler <afowler@bowdoin.edu>
2023-12-08 00:06:17 -0500

    Tweak probabilities, change method for averaging probabilities (no changes since 9/14)

--- map_generator.py
+++ map_generator.py
@@ -19,68 +19,68 @@ MAP_COLORS = {
 """ Contains the probablilities for "moving" from one terrain type to another """
 TRANSITION_MATRIX = {
     "grass": {
-        "grass": 0.905,
-        "sand": 0.045,
-        "water": 0.0,
-        "forest": 0.025,
-        "low mountain": 0.025,
+        "grass": 0.94,
+        "sand": 0.02,
+        "water": 0.01,
+        "forest": 0.015,
+        "low mountain": 0.015,
         "high mountain": 0.0
         },
     "sand": {
-        "grass": 0.1,
-        "sand": 0.82,
-        "water": 0.08,
+        "grass": 0.06,
+        "sand": 0.91,
+        "water": 0.03,
         "forest": 0.0,
         "low mountain": 0.0,
         "high mountain": 0.0
         },
     "water": {
-        "grass": 0.05,
-        "sand": 0.05,
-        "water": 0.9,
+        "grass": 0.02,
+        "sand": 0.02,
+        "water": 0.96,
         "forest": 0.0,
         "low mountain": 0.0,
         "high mountain": 0.0
         },
     "forest": {
-        "grass": 0.1,
+        "grass": 0.08,
         "sand": 0.0,
         "water": 0.0,
-        "forest": 0.9,
+        "forest": 0.92,
         "low mountain": 0.0,
         "high mountain": 0.0
         },
     "low mountain": {
-        "grass": 0.08,
-        "sand": 0.015,
+        "grass": 0.035,
+        "sand": 0.01,
         "water": 0.0,
         "forest": 0.0,
-        "low mountain": 0.805,
-        "high mountain": 0.1
+        "low mountain": 0.83,
+        "high mountain": 0.125
         },
     "high mountain": {
         "grass": 0.0,
         "sand": 0.0,
         "water": 0.0,
         "forest": 0.0,
-        "low mountain": 0.3,
-        "high mountain": 0.7
+        "low mountain": 0.25,
+        "high mountain": 0.75
         }
 }
 """ The width of the image to be generated in pixels """
-IMAGE_WIDTH = 1404
+IMAGE_WIDTH = 1400
 """ The height of the image to be generated in pixels """
-IMAGE_HEIGHT = 900
+IMAGE_HEIGHT = 904
 """ The length/height of each square of the grid in pixels """
-GRID_SIZE = 6
+GRID_SIZE = 8
 
 class MapGenerator:
     def __init__(self, transition_matrix):
         self.transition_matrix = transition_matrix
         self.terrain_types = list(transition_matrix)
         self.current_terrain = self.get_first_terrain()
-        self.current_x = 0 # in pixels
-        self.current_y = 0 # in pixels
+        self.current_x = 0 # in pixels, not referencing the grid
+        self.current_y = 0 # in pixels, not referencing the grid
 
         # set up image
         self.img = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
@@ -100,6 +100,7 @@ class MapGenerator:
         if self.current_x == 0:
             # we can just use the base probabilities for the first column, since there are no filled in pixels to the left
             next_terrain = numpy.random.choice(self.terrain_types, p=self.get_probabilities(self.current_terrain))
+            # WHEN STARTING A NEW COLUMN, DON'T RELY ON CURRENT_TERRAIN TO DECIDE NEW TERRAIN TODO
         else:
             # past the first column, we want to average the probabilities from the current pixel (before) and the pixel to the left
             new_probabilities = self.get_new_probabilities()
@@ -126,10 +127,34 @@ class MapGenerator:
         probs_before = self.get_probabilities(self.current_terrain)
 
         new_probs = []
+        num_zeroes = 0
+        leftover = 0
         for i in range(len(self.terrain_types)):
-            sum = probs_left[i] + probs_before[i]
-            new_probs.append(sum/2)
+            if probs_left[i] == 0 or probs_before[i] == 0:
+                new_probs.append(0)
+                num_zeroes += 1
+                leftover = leftover + (probs_left[i] + probs_before[i])/2
+            else:
+                sum = probs_left[i] + probs_before[i]
+                new_probs.append(sum/2)
+        
+        if num_zeroes > 0 and leftover > 0:
+            num_non_zeroes = len(self.terrain_types) - num_zeroes
+            if num_non_zeroes == 0:
+                # # no overlap between the two terrain types. Average them like normal.
+                # for i in range(len(new_probs)):
+                #     sum = probs_left[i] + probs_before[i]
+                #     new_probs[i] = (sum/2)
 
+                # no overlap between the two terrain types. prioritize current_terrain
+                new_probs = probs_before
+                # TODO make it randomly choose one
+            else:
+                # anything that is a 0% chance for one of the terrains but not for the other will be a 0% chance in the new probabilities TODO word this better
+                for i in range(len(new_probs)):
+                    if new_probs[i] != 0:
+                        new_probs[i] += leftover/num_non_zeroes # sand and hm have no overlap: probs will be all 0s
+        
         return new_probs
     
     def get_terrain_from_color(self, color):
@@ -145,11 +170,14 @@ class MapGenerator:
             for y in range(0, IMAGE_HEIGHT, GRID_SIZE):
                 self.current_y = y
                 current_pixel_color = self.img.getpixel((x+1,y+1))
-                if current_pixel_color[0] == current_pixel_color[1] == current_pixel_color[2] == 0:
+                if current_pixel_color[0] == current_pixel_color[1] == current_pixel_color[2] == 0: # if the current pixel hasn't been filled yet
                     next_terrain_color = MAP_COLORS[self.current_terrain]
-                    rand_int = numpy.random.randint(1, 2)
-                    self.imgdraw.rectangle([(x,y), (x+GRID_SIZE, y+(GRID_SIZE*rand_int))], fill=next_terrain_color)
+                    rand_int_x = numpy.random.randint(1, 2)
+                    rand_int_y = numpy.random.randint(1, 2) # TODO currently, these two variables do nothing. They roll a 1 every time. Just here for testing purposes.
+                    self.imgdraw.rectangle([(x,y), (x+(GRID_SIZE*rand_int_x), y+(GRID_SIZE*rand_int_y))], fill=next_terrain_color)
                     self.get_next_terrain()
+                
+                self.img.save(image_name) # TODO take this out. just here for testing purposes
 
         self.img.save(image_name)
 
